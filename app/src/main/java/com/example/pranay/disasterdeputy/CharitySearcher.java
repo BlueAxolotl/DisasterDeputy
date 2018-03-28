f24f3bbd15fa52deb5e4cbe2943c19acd9418299
victoriabowen <vabowen@wpi.edu>
2018-03-28 09:46:06 -0400

    I added the main activity for the user to choose if they are a charity or a donor.

--- app/src/main/java/com/example/pranay/disasterdeputy/CharitySearcher.java
+++ app/src/main/java/com/example/pranay/disasterdeputy/CharitySearcher.java
@@ -3,11 +3,11 @@ package com.example.pranay.disasterdeputy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RandomActivity extends AppCompatActivity {
+public class CharitySearcher extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_random);
+        setContentView(R.layout.activity_charity_searcher);
     }
 }
