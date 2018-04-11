a719d09c7a593eb6fbc303b6b02535da09acca4e
victoriabowen <vabowen@wpi.edu>
2018-04-11 16:35:04 -0400

    I added the main activity for the user to choose if they are a charity or a donor.

--- app/src/main/java/com/example/pranay/disasterdeputy/MainActivity.java
+++ app/src/main/java/com/example/pranay/disasterdeputy/MainActivity.java
@@ -15,40 +15,41 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<Charity> charitiesObject;
+   // ArrayList<Charity> charitiesObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       charitiesObject = new ArrayList<Charity>();
-        InputStream is = getResources().openRawResource(R.raw.originalcharitylist);
+//       charitiesObject = new ArrayList<Charity>();
+//        InputStream is = getResources().openRawResource(R.raw.originalcharitylist);
+//
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//
+//        String line = "";
+//
+//        try {
+//
+//
+//            while ((line = reader.readLine()) != null) {
+//
+//                String[] fields = line.split(",");
+//                ArrayList<String> supplies = new ArrayList<String>();
+//                Charity c = new Charity(fields[0], fields[1], supplies);
+//                charitiesObject.add(c);
+//
+//            }
+//        } catch (IOException e) {
+//            Log.e("MainActivity", "Error reading data on line" + line);
+//
+//        }
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
-        String line = "";
-
-        try {
-
-
-            while ((line = reader.readLine()) != null) {
-
-                String[] fields = line.split(",");
-                ArrayList<String> supplies = new ArrayList<String>();
-                Charity c = new Charity(fields[0], fields[1], supplies);
-                charitiesObject.add(c);
-
-            }
-        } catch (IOException e) {
-            Log.e("MainActivity", "Error reading data on line" + line);
-
-        }
     }
 
     //This method brings the user to the charity searcher class when the charity button is pressed
     public void CharityPush(View v){
-        Intent intent = new Intent (MainActivity.this, DonorSearcher.class);
-        Bundle bundle = new Bundle();
-        bundle.putParcelableArrayList("CharityList", (ArrayList<? extends Parcelable>) charitiesObject);
+        Intent intent = new Intent (this, CharitySearcher.class);
+
+        //intent.putExtra("CharityList", charitiesObject);
 
         startActivity(intent);
 
@@ -57,11 +58,13 @@ public class MainActivity extends AppCompatActivity {
 
     //This method switches to the Donor searcher class when the donor button is pressed
     public void DonorPush(View v){
-        Intent intent2 = new Intent (MainActivity.this, DonorSearcher.class);
-        Bundle bundle = new Bundle();
-        bundle.putParcelableArrayList("CharityList", (ArrayList<? extends Parcelable>) charitiesObject);
-        intent2.putExtras(bundle);
-        startActivity(intent2);
+        Log.d("MainActivity","In donor push function");
+        Intent intent = new Intent (this, DonorSearcher.class);
+
+       // intent.putExtra("CharityList", charitiesObject);
+
+        startActivity(intent);
+
 
 
     }
