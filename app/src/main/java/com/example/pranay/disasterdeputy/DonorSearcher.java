a6b434655e031585baef77bc285b07fc21b3fcc9
anika.gampa <anikagampa@gmail.com>
2018-04-10 22:54:24 -0400

    hey

--- app/src/main/java/com/example/pranay/disasterdeputy/DonorSearcher.java
+++ app/src/main/java/com/example/pranay/disasterdeputy/DonorSearcher.java
@@ -3,18 +3,28 @@ package com.example.pranay.disasterdeputy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DonorSearcher extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String charityList[]={"UNICEF", "American Red Cross"};
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donor_searcher);
-        ListView simpleList = (ListView) findViewById(R.id.CharityList_User);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_donor_searcher, charityList);
-        simpleList.setAdapter(arrayAdapter);
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_donor_searcher);
+        int charitytextfilelength = 10;
+
+        ArrayList<String> charities = new ArrayList<String>();
+        for(int i = 0; i < charitytextfilelength; i++){
+            charities.add(charities.get(i));
+        }
+        ListAdapter myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, charities);
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+        myListView.setAdapter(myAdapter);
     }
 }
