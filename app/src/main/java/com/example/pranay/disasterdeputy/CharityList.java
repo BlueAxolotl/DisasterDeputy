bcb58b346613216ab4ed1758475ecaf8ac1a92aa
victoriabowen <vabowen@wpi.edu>
2018-04-10 22:55:25 -0400

    I added the main activity for the user to choose if they are a charity or a donor.

--- app/src/main/java/com/example/pranay/disasterdeputy/CharityList.java
+++ app/src/main/java/com/example/pranay/disasterdeputy/CharityList.java
@@ -2,52 +2,68 @@ package com.example.pranay.disasterdeputy;
 //import com.google.firebase.database.DatabaseReference;
 //import com.google.firebase.database.FirebaseDatabase;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 /**
  * Created by Pranay on 4/2/18.
  */
 
+//This class is still in progress but it will eventually  be used to establish the database of charities
+
 public class CharityList {
-    public static void main(String[]args){
-        ArrayList<Charity> list = new ArrayList<Charity>();
+   // public static void main(String[]args){
+
         //FirebaseDatabase database = FirebaseDatabase.getInstance();
         //DatabaseReference myRef = database.getReference("message ");
         //myRef.setValue(list);
 
+//}
+
+
+
 
     }
-    private ArrayList<Charity> charities = new ArrayList<Charity>();
-    public static ArrayList<Charity> search(String s, ArrayList<Charity> charities){
-        ArrayList<Charity> matches = new ArrayList<Charity>();
-        for(int i = 0;i< charities.size();i++){
-            for(int j = 0; j<charities.get(i).getSupplies().size();j++){
-                if(charities.get(i).getSupplies().get(j).equals(s)){
-                    matches.add(charities.get(i));
-                }
-            }
-        }
-        return matches;
-    }
-    public static ArrayList<Charity> sort(ArrayList<Charity> matches){
-        ArrayList<Charity> sortedList = new ArrayList<Charity>();
-        if(matches.size()>0){
-            sortedList.add(matches.get(0));
-        }
-        for(int i = 1; i<matches.size();i++){
-            boolean added = false;
-            for(int j = 0;j<sortedList.size();j++){
-                if(matches.get(i).getDistance()<=sortedList.get(j).getDistance()){
-                    sortedList.add(j, matches.get(i));
-                    added = true;
-                    break;
-                }
+  //  private ArrayList<Charity> charities = new ArrayList<Charity>();
+    //public static ArrayList<Charity> search(String s, ArrayList<Charity> charities){
+      //  ArrayList<Charity> matches = new ArrayList<Charity>();
+        //for(int i = 0;i< charities.size();i++){
+          //  for(int j = 0; j<charities.get(i).getSupplies().size();j++){
+            //    if(charities.get(i).getSupplies().get(j).equals(s)){
+              //      matches.add(charities.get(i));
+                //}
+            //}
+        //}
+        //return matches;
+    //}
+    //public static ArrayList<Charity> sort(ArrayList<Charity> matches){
+      //  ArrayList<Charity> sortedList = new ArrayList<Charity>();
+        //if(matches.size()>0){
+          //  sortedList.add(matches.get(0));
+        //}
+        //for(int i = 1; i<matches.size();i++){
+          //  boolean added = false;
+            //for(int j = 0;j<sortedList.size();j++){
+              //  if(matches.get(i).getDistance()<=sortedList.get(j).getDistance()){
+                //    sortedList.add(j, matches.get(i));
+                  //  added = true;
+                    //break;
+                //}
 
-            }
-            if(!added){
-                sortedList.add(matches.get(i));
-            }
-        }
-        return sortedList;
-    }
+            //}
+            //if(!added){
+             //   sortedList.add(matches.get(i));
+            //}
+        //}
+        //return sortedList;
+    //}
 
-}
+
+
+
+
+
--- app/src/main/java/com/example/pranay/disasterdeputy/DonorSearcher.java
+++ app/src/main/java/com/example/pranay/disasterdeputy/DonorSearcher.java
@@ -2,19 +2,34 @@ package com.example.pranay.disasterdeputy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class DonorSearcher extends AppCompatActivity {
 
+
+    ListView simpleList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String charityList[]={"UNICEF", "American Red Cross"};
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_searcher);
-        ListView simpleList = (ListView) findViewById(R.id.CharityList_User);
+        Bundle extras = getIntent().getExtras();
+        ArrayList<Charity> Charities  =  (ArrayList<Charity>)getIntent().getSerializableExtra("CharityList");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_donor_searcher, charityList);
-        simpleList.setAdapter(arrayAdapter);
+
+
     }
+
+
 }
