61725f3ea5d8dd2aa34c27bee7ff8a7523e347f9
victoriabowen <vabowen@wpi.edu>
2018-04-13 09:21:48 -0400

    I added the main activity for the user to choose if they are a charity or a donor.

--- app/src/main/java/com/example/pranay/disasterdeputy/individualCharity.java
+++ app/src/main/java/com/example/pranay/disasterdeputy/individualCharity.java
@@ -2,6 +2,12 @@ package com.example.pranay.disasterdeputy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 //This class will be for the individual charity once the user clicks on it
 //The class will display the location and supplies that the charity needs
 public class individualCharity extends AppCompatActivity {
@@ -10,5 +16,12 @@ public class individualCharity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_charity);
+
+        ArrayList<String> supplies = new ArrayList<String>();
+
+        ListAdapter suppliesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, supplies);
+        ListView myListView = (ListView) findViewById(R.id.suppliesList);
+        myListView.setAdapter(suppliesAdapter);
+
     }
 }
