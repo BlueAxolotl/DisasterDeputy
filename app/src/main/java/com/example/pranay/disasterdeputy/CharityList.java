3ffbe797b890a046f758d3ea761ad21751b567c2
BlueAxolotl <pranay.pherwani@gmail.com>
2018-04-17 08:02:05 -0400

    Commit

--- app/src/main/java/com/example/pranay/disasterdeputy/CharityList.java
+++ app/src/main/java/com/example/pranay/disasterdeputy/CharityList.java
@@ -1,6 +1,9 @@
 package com.example.pranay.disasterdeputy;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.util.Log;
 
@@ -16,51 +19,73 @@ import java.util.ArrayList;
 //This class is still in progress but it will eventually  be used to establish the database of charities
 
 public class CharityList {
-   // public static void main(String[]args){
+    static DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    Charity result;
+    public static void main(String[]args){
+        ArrayList<Charity> charities = new ArrayList<Charity>();
+        for(int i = 0;i<charities.size();i++) {
 
-        //FirebaseDatabase database = FirebaseDatabase.getInstance();
-        //DatabaseReference myRef = database.getReference("message ");
+            DatabaseReference pushedCharity = database.child(charities.get(i).getName());
+            pushedCharity.setValue(charities.get(i));
+        }
+
         //myRef.setValue(list);
-
-//}
-
-
-
-
     }
-  //  private ArrayList<Charity> charities = new ArrayList<Charity>();
-    //public static ArrayList<Charity> search(String s, ArrayList<Charity> charities){
-      //  ArrayList<Charity> matches = new ArrayList<Charity>();
-        //for(int i = 0;i< charities.size();i++){
-          //  for(int j = 0; j<charities.get(i).getSupplies().size();j++){
-            //    if(charities.get(i).getSupplies().get(j).equals(s)){
-              //      matches.add(charities.get(i));
-                //}
-            //}
-        //}
-        //return matches;
-    //}
-    //public static ArrayList<Charity> sort(ArrayList<Charity> matches){
-      //  ArrayList<Charity> sortedList = new ArrayList<Charity>();
-        //if(matches.size()>0){
-          //  sortedList.add(matches.get(0));
-        //}
-        //for(int i = 1; i<matches.size();i++){
-          //  boolean added = false;
-            //for(int j = 0;j<sortedList.size();j++){
-              //  if(matches.get(i).getDistance()<=sortedList.get(j).getDistance()){
-                //    sortedList.add(j, matches.get(i));
-                  //  added = true;
-                    //break;
-                //}
+    public Charity getCharityByName(String name) {
 
-            //}
-            //if(!added){
-             //   sortedList.add(matches.get(i));
-            //}
-        //}
-        //return sortedList;
-    //}
+        database.child("charities").child(name).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                result = dataSnapshot.getValue(Charity.class);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return result;
+    }
+
+
+
+
+
+    private ArrayList<Charity> charities = new ArrayList<Charity>();
+    public static ArrayList<Charity> search(String s, ArrayList<Charity> charities){
+        ArrayList<Charity> matches = new ArrayList<Charity>();
+        for(int i = 0;i< charities.size();i++){
+            for(int j = 0; j<charities.get(i).getSupplies().size();j++){
+                if(charities.get(i).getSupplies().get(j).equals(s)){
+                    matches.add(charities.get(i));
+                }
+            }
+        }
+        return matches;
+    }
+    public static ArrayList<Charity> sort(ArrayList<Charity> matches){
+        ArrayList<Charity> sortedList = new ArrayList<Charity>();
+        if(matches.size()>0){
+            sortedList.add(matches.get(0));
+        }
+        for(int i = 1; i<matches.size();i++){
+            boolean added = false;
+            for(int j = 0;j<sortedList.size();j++){
+                if(matches.get(i).getDistance()<=sortedList.get(j).getDistance()){
+                    sortedList.add(j, matches.get(i));
+                    added = true;
+                    break;
+                }
+
+            }
+            if(!added){
+                sortedList.add(matches.get(i));
+            }
+        }
+        return sortedList;
+    }
+}
 
 
 
